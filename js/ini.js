52a2acef8da3fbd7db8379c4032866770e555457
AzovUA <110759260+AzovUA@users.noreply.github.com>
2023-11-13 04:56:21 +0200

    adds JQuery and the "slick js" library

---
+++ js/ini.js
@@ -0,0 +1,68 @@
+$(function(){
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop;
+
+    // Fixed-Header
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if( scrollOffset >= introH) {
+            header.addClass("fixed");
+        } else{
+            header.removeClass("fixed");
+        }
+    }
+
+    // Smooth scroll
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+        
+        $("nav a").removeClass("active")
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+    
+
+    // Nav
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass('active')
+        $("#nav").toggleClass('active')
+    });
+
+    // Collapse
+    $("[data-collapse]").on("click", function(event){
+        event.preventDefault()
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+        
+        $this.toggleClass("active");
+    });
+
+    //Slider
+
+    $("[data-slider]").slick({
+        Infinity: true,
+        fade: false,
+        sliderToShow: 1,
+        sliderToScroll: 1
+    });
+
+});
